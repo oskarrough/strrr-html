0a2542ec5f91c6c27719554a13b7cf985d00c455
Oskar Rough <oskar@rough.dk>
2024-11-18 14:28:06 +0100

    Rework the data loading

--- _data/seasons.js
+++ _data/seasons.js
@@ -1,28 +1,13 @@
-const shows = require('../shows.json')
+import shows from "./shows.js";
 
-function flatten(show) {
-	const s = Object.assign(show, show.attributes)
-	delete s.attributes
-	return s
-}
+const showsBySeason = (season) =>
+	shows.filter((show) => show.season_number === season);
 
-function filterBy(list, prop, val) {
-	return list.filter(item => {
-		return item[prop] === val
-	})
-}
-
-module.exports = function () {
-	const realShows = shows.data
-		.map(flatten)
-		.filter((show) => show['is-published'])
-		.sort((a, b) => a['episode-number'] - b['episode-number'])
-	return [
-		filterBy(realShows, 'season-number', 1),
-		filterBy(realShows, 'season-number', 2),
-		filterBy(realShows, 'season-number', 3),
-		filterBy(realShows, 'season-number', 4),
-		filterBy(realShows, 'season-number', 5),
-		filterBy(realShows, 'season-number', 6)
-	]
-}
+export default [
+	showsBySeason(1),
+	showsBySeason(2),
+	showsBySeason(3),
+	showsBySeason(4),
+	showsBySeason(5),
+	showsBySeason(6),
+];
--- _data/shows.js
+++ _data/shows.js
@@ -1,21 +1,5 @@
-const shows = require('../shows.json')
+import shows from "./shows-raw.json" with { type: "json" };
 
-function flatten(show) {
-	const s = Object.assign(show, show.attributes)
-	delete s.attributes
-	return s
-}
-
-function filterBy(list, prop, val) {
-	return list.filter(item => {
-		return item[prop] === val
-	})
-}
-
-module.exports = function () {
-	const realShows = shows.data
-		.map(flatten)
-		.filter((show) => show['is-published'])
-		.sort((a, b) => a['episode-number'] - b['episode-number'])
-	return realShows
-}
+export default shows
+	.filter((show) => show.is_published)
+	.sort((a, b) => a.episode_number - b.episode_number);
