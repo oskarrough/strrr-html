5bae6a17969fcd86a900fdbd894f6055659e4d53
Oskar Rough <oskar@rough.dk>
2025-05-24 14:38:09 +0200

    Fix data loading of shows and videos

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ all:
 	make shows.json videos.json
 
 shows.json:
-	curl https://strrr-phoenix.herokuapp.com/api/shows/ > shows.json
+	curl https://api.strrr.tv/api/shows/ > _data/shows-raw.json
 
 videos.json:
-	curl https://strrr-phoenix.herokuapp.com/api/videos/ > videos.json
+	curl https://api.strrr.tv/api/videos/ > _data/videos-raw.json
--- _data/shows.js
+++ _data/shows.js
@@ -1,5 +1,14 @@
-import shows from "./shows-raw.json" with { type: "json" };
+import shows from "./shows-raw.json" with { type: "json" }
+import videos from "./videos-raw.json" with { type: "json" }
 
-export default shows
+export default shows.data
 	.filter((show) => show.is_published)
-	.sort((a, b) => a.episode_number - b.episode_number);
+	.sort((a, b) => a.episode_number - b.episode_number)
+	.map((show) => {
+		return {
+			...show,
+			videos: videos.data
+				.filter(video => video.show_id === show.id)
+				.sort((a, b) => a.order - b.order)
+		}
+	})
