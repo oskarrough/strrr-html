2c6be4807b94b4e25b300861cbc752b3d6d432c9
oskarrough <oskar@rough.dk>
2025-05-25 14:07:53 +0200

    Refactor data

--- _data/shows.js
+++ _data/shows.js
@@ -1,10 +1,8 @@
 import shows from "./shows-raw.json" with { type: "json" }
-import videos from "./videos-raw.json" with { type: "json" }
 import playlists from "./playlists-raw.json" with { type: "json" }
+// import videos from "./videos-raw.json" with { type: "json" }
 
-// console.log(playlists)
-
-/* Published shows with embedded videos */
+/* Published shows with embedded playlist + videos */
 export default shows.data
 	.filter((show) => show.is_published)
 	.sort((a, b) => a.episode_number - b.episode_number)
@@ -12,12 +10,18 @@ export default shows.data
 		const playlist = playlists.find(list => 
 			list.season_number === show.season_number
 			&& list.episode_number === show.episode_number)
+
+			if (!playlist) {
+				console.warn(`Playlist not found for show ${show.title}`)
+			}
+
 		return {
 			...show,
-			playlist,
-			// playlist_id: playlists[a.season_number].find(episode => episode.episode_number === show.episode_number).playlist_id,
-			videos: videos.data
-				.filter(video => video.show_id === show.id)
-				.sort((a, b) => a.order - b.order)
+			playlist_id: playlist?.playlist_id,
+			video_id: playlist?.video_id,
+			// playlist,
+			// videos: videos.data
+			// 	.filter(video => video.show_id === show.id)
+			// 	.sort((a, b) => a.order - b.order)
 		}
 	})
