e940a5ffc12b88fde53a1ae50d94602d62c2da5a
oskarrough <oskar@rough.dk>
2025-05-25 11:52:37 +0200

    Sort seasons by newest first

--- _data/seasons.js
+++ _data/seasons.js
@@ -4,10 +4,10 @@ const showsBySeason = (season) =>
 	shows.filter((show) => show.season_number === season);
 
 export default [
-	showsBySeason(1),
-	showsBySeason(2),
-	showsBySeason(3),
-	showsBySeason(4),
-	showsBySeason(5),
 	showsBySeason(6),
+	showsBySeason(5),
+	showsBySeason(4),
+	showsBySeason(3),
+	showsBySeason(2),
+	showsBySeason(1),
 ];
